02d3cb422d3376a3904682ba00475c74572f0b2a
Eric Jefferson Jr <Ejefferson50@gmail.com>
2020-05-10 05:11:27 +0000

    Added Signup Form

--- src/components/LoginContainer/Signup.js
+++ src/components/LoginContainer/Signup.js
@@ -3,18 +3,45 @@ import LoginButton from '../ButtonContainer/LoginButton';
 import CancelButton from '../ButtonContainer/CancelButton';
 
 
-function Signup(){
+function Signup() {
 
-    
-            return(
-                <div className="signup">
-                <h1>This is the Signup Screen</h1>
+
+    return (
+        <div className="signup">
+            <h1>Sign Up</h1>
+            <form>
+                <div className="form-group">
+                    <label>Firstname:</label>
+                    <input type="text" className="form-control" placeholder="" />
+                </div>
+                <div className="form-group">
+                    <label>Lastname:</label>
+                    <input type="text" className="form-control" placeholder="" />
+                </div>
+                <div className="form-group">
+                    <label>Username:</label>
+                    <input type="text" className="form-control" placeholder="" />
+                </div>
+                **Username may not exceed 15 characters**
+                <div className="form-group">
+                    <label>Email:</label>
+                    <input type="text" className="form-control" placeholder="" />
+                </div>
+                <div className="form-group">
+                    <label>Password:</label>
+                    <input type="text" className="form-control" placeholder="" />
+                </div>
+                **Password must at least contain 8 characters (must include Uppercase, Lowercase, Numbers, Special Characters)**
+                </form>
                 <LoginButton />
                 <CancelButton />
-                </div>
-            );
+                <a href="#">Terms and Conditions</a>
+        </div>
+        
 
-    
+    );
+
+
 
 }
 
